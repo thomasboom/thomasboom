b77d7b0d469cd2597dd79b6531ec1b99963293c4
ThomasNow Productions <thomasnowprod@proton.me>
2026-01-29 20:32:55 +0100

    Update posts.ts

--- website/app/blog/posts.ts
+++ website/app/blog/posts.ts
@@ -7,14 +7,20 @@ export interface Post {
 
 export const posts: Post[] = [
   {
-    slug: 'how-making-bad-decisions-for-the-people-does-not-impact-businesses-long-term',
-    title: 'How making bad decisions for the people does not impact businesses long term.',
-    date: '2026-01-26',
-    content: `Every time there is a major hiccup in tech, people are mad. Really mad. When we look back at the massive Reddit "shutdown" of 2023, where [over 8000 subreddits turned black after Reddit started charging for API access](https://techcrunch.com/2023/06/12/reddit-blackout-8000-subreddits-went-dark-protest-api/), this was one of the largest internet boycotts ever. But now, less than 2 years later, engagement is back to where it was before, and literally nothing changed, other than that some subreddits are now moderated by the official Reddit team, because they seized control of them. How can we accept this? Companies should take people over profit, not the other way around. The main problem is that all the people are already on the large platforms, and that it is hard to migrate them. Even when we look at one of the largest internet moves, when Elon Musk acquired X (then still good old Twitter), [a lot of people moved to Mastodon and Bluesky, which technically are way better for the users](https://www.theverge.com/23686584/twitter-alternative-social-media-platforms-mastodon-bluesky-activitypub-protocol) and less for (nonexistent) investors. They still don't have any kind of power against X, even though it has become one of the most toxic places on the internet.
+    slug: 'my-mastodon-setup',
+    title: 'My Mastodon setup',
+    date: '2026-01-28',
+    content: `After deciding to switch from [Bluesky](https://bsky.app) to [Mastodon](https://joinmastodon.org/), here's how I set everything up.
   
-  The main takeaway? Big tech has too much power over its users, and even if there are problems, people will stick anyway. Take action. Switch to platforms like [Bluesky](https://bsky.app) and [Nooki](https://nooki.me/). Take action, don't let big tech control you.`
+  How did the move go? Because the protocols differ, there's no import/export yet, so I did it manually. Surprisingly, lots of people and communities already mirror to Mastodon, either by hand or via [Bridgy Fed](https://fed.brid.gy), though plenty of personal accounts still aren't there.
+  
+  I registered on [mastodon.social](https://mastodon.social), the largest server. Sign-up was straightforward, but the UI had less polish and it wouldn't accept my [addy.io](https://addy.io) email alias.
+  
+  As for the UI/UX, I now use [elk.zone](https://elk.zone), a FOSS third-party frontend with great UI/UX.
+  
+  And the rest? I'll post a longer-term review after I've used the platform for a few days.`
   },
-{
+  {
     slug: 'why-i-switched-to-mastodon',
     title: 'Why I switched to Mastodon',
     date: '2026-01-28',
@@ -29,17 +35,11 @@ export const posts: Post[] = [
   Fourth: interoperability. Because Mastodon is part of the Fediverse, it can talk to many other social networks, so I can see content from [Threads](https://threads.net), [Loops](https://joinloops.org) and [Pixelfed](https://pixelfed.org).`
   },
   {
-    slug: 'my-mastodon-setup',
-    title: 'My Mastodon setup',
-    date: '2026-01-28',
-    content: `After deciding to switch from [Bluesky](https://bsky.app) to [Mastodon](https://joinmastodon.org/), here's how I set everything up.
+    slug: 'how-making-bad-decisions-for-the-people-does-not-impact-businesses-long-term',
+    title: 'How making bad decisions for the people does not impact businesses long term.',
+    date: '2026-01-26',
+    content: `Every time there is a major hiccup in tech, people are mad. Really mad. When we look back at the massive Reddit "shutdown" of 2023, where [over 8000 subreddits turned black after Reddit started charging for API access](https://techcrunch.com/2023/06/12/reddit-blackout-8000-subreddits-went-dark-protest-api/), this was one of the largest internet boycotts ever. But now, less than 2 years later, engagement is back to where it was before, and literally nothing changed, other than that some subreddits are now moderated by the official Reddit team, because they seized control of them. How can we accept this? Companies should take people over profit, not the other way around. The main problem is that all the people are already on the large platforms, and that it is hard to migrate them. Even when we look at one of the largest internet moves, when Elon Musk acquired X (then still good old Twitter), [a lot of people moved to Mastodon and Bluesky, which technically are way better for the users](https://www.theverge.com/23686584/twitter-alternative-social-media-platforms-mastodon-bluesky-activitypub-protocol) and less for (nonexistent) investors. They still don't have any kind of power against X, even though it has become one of the most toxic places on the internet.
   
-  How did the move go? Because the protocols differ, there's no import/export yet, so I did it manually. Surprisingly, lots of people and communities already mirror to Mastodon, either by hand or via [Bridgy Fed](https://fed.brid.gy), though plenty of personal accounts still aren't there.
-  
-  I registered on [mastodon.social](https://mastodon.social), the largest server. Sign-up was straightforward, but the UI had less polish and it wouldn't accept my [addy.io](https://addy.io) email alias.
-  
-  As for the UI/UX, I now use [elk.zone](https://elk.zone), a FOSS third-party frontend with great UI/UX.
-  
-  And the rest? I'll post a longer-term review after I've used the platform for a few days.`
+  The main takeaway? Big tech has too much power over its users, and even if there are problems, people will stick anyway. Take action. Switch to platforms like [Bluesky](https://bsky.app) and [Nooki](https://nooki.me/). Take action, don't let big tech control you.`
   }
 ];
