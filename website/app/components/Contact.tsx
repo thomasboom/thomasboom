907ff6a84d1fc2e5ce6cc91bc16ac3b1fcd3a935
Thomas Boom <thomasnowprod@proton.me>
2026-04-06 07:58:52 +0200

    Added GitHub account back because it's now unbanned.

--- website/app/components/Contact.tsx
+++ website/app/components/Contact.tsx
@@ -1,7 +1,7 @@
 export default function Contact() {
   const contacts = [
     { label: 'Email', href: 'mailto:thomasnowprod@proton.me' },
-    // { label: "GitHub", href: "https://github.com/thomasboom" },
+    { label: "GitHub", href: "https://github.com/thomasboom" },
     { label: 'Codeberg', href: 'https://codeberg.org/thomasboom' },
     { label: 'Signal', href: 'https://signal.me/#eu/fEo_ZOMCPYGFE3QyEDyQy3c3Jz8hWsmpTLFxW2CHRQwVehMlgjhNKKKUX3LtsDcQ' },
     { label: 'Mastodon', href: 'https://mastodon.social/@thomasboom' },
